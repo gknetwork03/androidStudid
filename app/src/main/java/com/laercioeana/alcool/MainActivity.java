552f855639a3a2aa31b9fc66afd3d8ac80bb765d
Edson Gabriel <gknetwork03@outlook.com.br>
2023-07-11 08:34:51 -0400

    first commit

---
+++ app/src/main/java/com/laercioeana/alcool/MainActivity.java
@@ -0,0 +1,52 @@
+package com.laercioeana.alcool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    float Gasolina = 0;
+    float Alcool = 0;
+    float x = 0;
+
+
+    EditText idGasolina,idAlcool;
+    TextView idResultado ;
+    Button idVerificar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        idGasolina = (EditText)  findViewById(R.id.idGasolina);
+        idAlcool = (EditText)  findViewById(R.id.idAlcool);
+        idResultado = (TextView) findViewById(R.id.idResultado);
+        idVerificar = (Button) findViewById(R.id.idVerificar);
+
+        idVerificar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View idVerificar) {
+                Alcool = Float.parseFloat(idAlcool.getText().toString());
+                Gasolina = Float.parseFloat(idGasolina.getText().toString());
+
+                   x = Alcool / Gasolina;
+
+                  if(x <= 0.7){
+                      idResultado.setText(" Melhor opção Alcool");
+
+                  }else {
+                      idResultado.setText("Melhor opção gasolina");
+                  }
+            }
+        });
+
+
+
+    }
+}
